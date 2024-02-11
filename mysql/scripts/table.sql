240421f9328b4bc1860d8c5fe7be47b6e7f635f5
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-02-11 11:11:04 +0900

    テーブルの内容を変更

--- mysql/scripts/table.sql
+++ mysql/scripts/table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `XserverVPS` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-    `time` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
-    `result` int NOT NULL DEFAULT 0,
+    `date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+    `response_code` int NOT NULL DEFAULT 0,
+    `response_time` double NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
