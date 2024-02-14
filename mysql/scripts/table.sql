43290fe8c1b370d4c782de01874337cbb9370c73
ma2kzzzz <92625267+matsukz@users.noreply.github.com>
2024-02-14 23:13:27 +0900

    PINGコンテナ追加に伴いテーブル変更

--- mysql/scripts/table.sql
+++ mysql/scripts/table.sql
@@ -1,8 +1,17 @@
-CREATE TABLE `XserverVPS` (
+CREATE TABLE `HTTP-phpmyadmin` (
     `id` bigint unsigned NOT NULL AUTO_INCREMENT,
     `date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
     `time` time NOT NULL DEFAULT '00:00:00',
     `response_code` int NOT NULL DEFAULT 0,
+    `response_time` double NOT NULL DEFAULT 0,
+    `response_result` boolean NOT NULL DEFAULT false,
+    PRIMARY KEY (`id`) 
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3;
+
+CREATE TABLE `PING-MySQL` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
+    `time` time NOT NULL DEFAULT '00:00:00',
     `response_time` double NOT NULL DEFAULT 0,
     `response_result` boolean NOT NULL DEFAULT false,
     PRIMARY KEY (`id`) 
